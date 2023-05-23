9b2d5c08340e978b510967a642ad561899568017
atticus-trellix <117859119+atticus-trellix@users.noreply.github.com>
2023-05-23 13:14:49 -0700

    idk bro

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
   name   = "blog"
-  vpc_id  = module.vpc.vpc_id
+  vpc_id  = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
