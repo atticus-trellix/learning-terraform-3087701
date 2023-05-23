0311edea76df928c6ac9f640c01cf25e40cbb5ad
atticus-trellix <117859119+atticus-trellix@users.noreply.github.com>
2023-05-23 13:47:35 -0700

    fuck

--- main.tf
+++ main.tf
@@ -20,10 +20,11 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a","us-west-2b"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
+  one_nat_gateway_per_az = true 
 
   tags = {
     Terraform = "true"
@@ -42,20 +43,6 @@ resource "aws_instance" "blog" {
   }
 }
 
-resource "aws_route_table" "blog_rt" {
-  vpc_id = module.blog_vpc.vpc_id
-
-  route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = module.blog_vpc.public_subnets[0]
-  }
-
-  tags = {
-    Name = "blog-rt-test"
-  }
-}
-
-
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
@@ -67,3 +54,4 @@ module "blog_sg" {
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
+
